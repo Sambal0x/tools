2e3b9f968c572422685a369041961b4c9c9d662f
Sambal0x <34885356+Sambal0x@users.noreply.github.com>
2019-10-23 01:39:28 +1100

    Update subdomain-quickie.sh

--- subdomain-quickie.sh
+++ subdomain-quickie.sh
@@ -2,7 +2,7 @@
 
 # Author: Sambal0x
 #
-# Instructions : ./subdomain-quickie.sh <domain.com>
+# Instructions : ./subdomain-quickie <domain.com>
 
 
 ###############################################  Subdomain Enumeration   ############################################
@@ -17,22 +17,28 @@ echo "[+] Bruteforcing $1 with massdns ..."
 
 cat $1-massdns.txt | cut -d " " -f 1 | sed 's/.$//' | sed '/\*/d' >> $1-subdomains.txt
 sort -u -o $1-subdomains.txt $1-subdomains.txt  # remove dups in files
+echo "[+] Found $(cat $1-subdomains.txt| wc -l) so far ..."
 
 # Run dnsgen to get MORE subdomains
 echo "[+] Bruteforcing $1 with dnsgen + massdns ..."
 cat $1-subdomains.txt | dnsgen - | massdns -r /opt/external/osint/massdns/lists/working-resolvers.txt -t A -o S -w $1-dnsgen.txt 2>/dev/null
 cat $1-dnsgen.txt | cut -d " " -f 1 | sed 's/.$//' | sed '/\*/d' >> $1-subdomains.txt
 sort -u -o $1-subdomains.txt $1-subdomains.txt  # remove dups in files
+echo "[+] Found $(cat $1-subdomains.txt| wc -l) so far ..."
 
 #############################################  Check for subdomain takeovers ########################################
 
 # Analyse for Subdomain takeover
-touch subd-takeover.txt
-echo "[+] We have $(cat $1-subdomains.txt| wc -l) subdomains.. Now analysing results with Subjack..."
-for i in $1-subdomains.txt; do /root/go/bin/subjack -w $i -c /root/go/src/github.com/haccer/subjack/fingerprints.json -o subd-takeover.txt; done 
+echo "[+] Now analysing results with Subjack..."
+for i in $1-subdomains.txt; do /root/go/bin/subjack -w $i -c /root/go/src/github.com/haccer/subjack/fingerprints.json -o $1-stakeover.txt; done 
 
 # Send results to slack's web hook
-# echo "[+] Sending results to slack ..."
-# var="$(cat subd-takeover.txt)"
-# curl -X POST -H 'Content-type: application/json' --data "{'text':'## Subdomain Takeover Results ##\n$var'}" \
-#	https://hooks.slack.com/services/blahblahblah
+echo "[+] Sending results to slack ..."
+
+subdomains="$(cat $1-subdomains.txt)"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'## Subdomain for $1 ##\n$subdomains'}" \
+	https://hooks.slack.com/services/blahblahblah
+
+takeover="$(cat $1-stakeover.txt)"
+curl -X POST -H 'Content-type: application/json' --data "{'text':'## Subdomain Takeover $1 ##\n$takeover'}" \
+	https://hooks.slack.com/services/blahblahblah
