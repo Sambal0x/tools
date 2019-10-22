88748576fce241d1345a022185de37bcad794cf9
Sambal0x <34885356+Sambal0x@users.noreply.github.com>
2019-10-23 01:22:10 +1100

    Create subdomain-quickie.sh
    Dirty script to enumerate subdomains for a target

---
+++ subdomain-quickie.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Author: Sambal0x
+#
+# Instructions : ./subdomain-quickie.sh <domain.com>
+
+
+###############################################  Subdomain Enumeration   ############################################
+
+echo "[+] Running Amass on $1..."
+amass enum -d $1 -o $1-subdomains.txt
+
+# Massdns subdomain bruteforce
+echo "[+] Bruteforcing $1 with massdns ..."
+/opt/external/osint/massdns/scripts/subbrute.py /opt/external/osint/massdns/lists/names.txt $1 \
+| massdns -r /opt/external/osint/massdns/lists/working-resolvers.txt -t A -o S -w $1-massdns.txt 2>/dev/null
+
+cat $1-massdns.txt | cut -d " " -f 1 | sed 's/.$//' | sed '/\*/d' >> $1-subdomains.txt
+sort -u -o $1-subdomains.txt $1-subdomains.txt  # remove dups in files
+
+# Run dnsgen to get MORE subdomains
+echo "[+] Bruteforcing $1 with dnsgen + massdns ..."
+cat $1-subdomains.txt | dnsgen - | massdns -r /opt/external/osint/massdns/lists/working-resolvers.txt -t A -o S -w $1-dnsgen.txt 2>/dev/null
+cat $1-dnsgen.txt | cut -d " " -f 1 | sed 's/.$//' | sed '/\*/d' >> $1-subdomains.txt
+sort -u -o $1-subdomains.txt $1-subdomains.txt  # remove dups in files
+
+#############################################  Check for subdomain takeovers ########################################
+
+# Analyse for Subdomain takeover
+touch subd-takeover.txt
+echo "[+] We have $(cat $1-subdomains.txt| wc -l) subdomains.. Now analysing results with Subjack..."
+for i in $1-subdomains.txt; do /root/go/bin/subjack -w $i -c /root/go/src/github.com/haccer/subjack/fingerprints.json -o subd-takeover.txt; done 
+
+# Send results to slack's web hook
+# echo "[+] Sending results to slack ..."
+# var="$(cat subd-takeover.txt)"
+# curl -X POST -H 'Content-type: application/json' --data "{'text':'## Subdomain Takeover Results ##\n$var'}" \
+#	https://hooks.slack.com/services/blahblahblah
